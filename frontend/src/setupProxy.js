91c1acd85643f24bd173d2e92c3610a610d0dbf4
Jon Dalberg <jon@releasehub.com>
2021-10-22 12:23:45 -0500

    use backend env var

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
+  const api = process.env.BACKEND_INGRESS_URL || "http://backend:8000";
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://backend:8000",
+      target: api,
       pathRewrite: { "^/api": "" }
     })
   );
